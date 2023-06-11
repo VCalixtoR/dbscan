d356ddbb45490355f7c05904d11d3e8a323c143c
VCalixtoR <viniciuscalixto.prof@gmail.com>
2023-06-11 12:08:50 -0300

    feat: output configuration for clustering

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,2 +1,14 @@
 pub type Point = Vec<f32>; // Each Point is an item of the dataset
 pub type PointVector = Vec<Point>; // PointVector is a collection of dataset items
+
+// single cluster
+pub struct Cluster {
+    pub cluster_name: String,
+    pub core_indexes: Vec<usize>,
+    pub border_indexes: Vec<usize>,
+}
+// result from clustering algorithm
+pub struct ClusterGroup {
+    pub clusters: Vec<Cluster>,
+    pub outlier_indexes: Vec<usize>,
+}
